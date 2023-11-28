d5bc2f38e8d7149fb997bd9e79ca8b7da21e80ae
admiralGeneralCP5 <149021332+admiralGeneralCP5@users.noreply.github.com>
2023-11-27 21:54:22 -0500

    Add files via upload

---
+++ toolinstaller.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt update
+
+packages=(btop kate realtek-rtl88xxau-dkms)
+
+# @ is needed in order to get all elemets of the array
+for package in ${packages[@]}; do
+	echo -e "***Getting $package..."
+	sudo apt install $package
+	echo
+	echo
+done
