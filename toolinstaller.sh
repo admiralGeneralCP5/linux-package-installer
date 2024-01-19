e53d86b1555f16c3876a60e6aa682de55e5c3fd9
admiralGeneralCP5 <149021332+admiralGeneralCP5@users.noreply.github.com>
2024-01-19 17:14:24 -0500

    Added konsole for kate and two more tools.

--- toolinstaller.sh
+++ toolinstaller.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-packages=(btop kate bat realtek-rtl88xxau-dkms)
+packages=(btop kate bat realtek-rtl88xxau-dkms konsole peass gobuster)
 
 # @ is needed in order to get all elemets of the array
 for package in ${packages[@]}; do
